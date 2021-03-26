0eb6543408eef04e588b70f4a64dca176d45e263
yupix <yupi0982@outlook.jp>
2021-03-27 01:56:05 +0900

    chore: uniqueを指定してなかった

--- app/model/hash.py
+++ app/model/hash.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String, Sequence, UniqueConstraint
 
 from app import Base
 
@@ -8,6 +8,7 @@ class Hash(Base):
 	HashModel
 	"""
 	__tablename__ = 'hashes'
+	__table_args__ = (UniqueConstraint('file_name', 'md5'),)
 	id = Column(Integer, Sequence('server_id_seq'), primary_key=True)
 	file_name = Column(String(60))
 	md5 = Column(String(60))
