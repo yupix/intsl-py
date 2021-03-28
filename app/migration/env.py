d6c7c6bdfac6fb337087f477a5776d2fe2c06da8
yupix <yupi0982@outlook.jp>
2021-03-28 23:45:48 +0900

    fix: sqliteでエラーが出るので対策

--- app/migration/env.py
+++ app/migration/env.py
@@ -21,8 +21,8 @@ fileConfig(config.config_file_name)
 # 読み込むモデル一覧
 
 target_models = [
-	'model.server',
-	'model.hash',
+    'model.server',
+    'model.hash',
 ]
 
 
@@ -31,16 +31,16 @@ target_models = [
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 def combine_metadata(*args):
-	m = MetaData()
-	for metadata in args:
-		for t in metadata.tables.values():
-			t.tometadata(m)
-	return m
+    m = MetaData()
+    for metadata in args:
+        for t in metadata.tables.values():
+            t.tometadata(m)
+    return m
 
 
 for target in target_models:
-	model = import_module(f'{target}')
-	target_metadata = combine_metadata(model.Base.metadata)
+    model = import_module(f'{target}')
+    target_metadata = combine_metadata(model.Base.metadata)
 
 
 # other values from the config, defined by the needs of env.py,
@@ -50,47 +50,48 @@ for target in target_models:
 
 
 def run_migrations_offline():
-	"""Run migrations in 'offline' mode.
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-    Calls to context.execute() here emit the given string to the
-    script output.
-    """
-	url = config.get_main_option("sqlalchemy.url")
-	context.configure(
-		url=url,
-		target_metadata=target_metadata,
-		literal_binds=True,
-		dialect_opts={"paramstyle": "named"},
-	)
+    """Run migrations in 'offline' mode.
+This configures the context with just a URL
+and not an Engine, though an Engine is acceptable
+here as well.  By skipping the Engine creation
+we don't even need a DBAPI to be available.
+Calls to context.execute() here emit the given string to the
+script output.
+"""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        render_as_batch=True,
+    )
 
-	with context.begin_transaction():
-		context.run_migrations()
+    with context.begin_transaction():
+        context.run_migrations()
 
 
 def run_migrations_online():
-	"""Run migrations in 'online' mode.
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-    """
-	connectable = engine_from_config(
-		config.get_section(config.config_ini_section),
-		prefix="sqlalchemy.",
-		poolclass=pool.NullPool,
-	)
+    """Run migrations in 'online' mode.
+In this scenario we need to create an Engine
+and associate a connection with the context.
+"""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
 
-	with connectable.connect() as connection:
-		context.configure(
-			connection=connection, target_metadata=target_metadata, compare_type=True
-		)
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata, compare_type=True
+        )
 
-		with context.begin_transaction():
-			context.run_migrations()
+        with context.begin_transaction():
+            context.run_migrations()
 
 
 if context.is_offline_mode():
-	run_migrations_offline()
+    run_migrations_offline()
 else:
-	run_migrations_online()
+    run_migrations_online()
